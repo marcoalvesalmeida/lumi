f29000c5778fb943db37796a32c9a67881ef48a6
Marco Almeida <marcoalvesalmeida>
2024-05-23 09:42:32 -0300

    feat: create swagger docs

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,25 @@
 import express from "express";
 import logger from "morgan";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger/output.json";
 
 import { router } from "./routes";
+import bodyParser from "body-parser";
 
 const PORT = 3333;
 
 const app = express();
 
 app.use(logger("dev"));
-app.use(express.json());
+app.use(bodyParser.json());
 
-app.use("/", router);
+app.use("/api/", router);
+
+app.use(
+    "/api/docs",
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerFile)
+);
 
 app.listen(PORT, () => {
     console.log(`Server listening at port: ${PORT}`);
