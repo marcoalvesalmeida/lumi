6e1b0cdc5fb788cc804ed6ff84304e16dd7ec741
Marco Almeida <marcoalvesalmeida>
2024-05-26 10:34:21 -0300

    refactor: fix dev dependencies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,29 +1,26 @@
-import express from "express";
+import express from 'express';
 import cors from 'cors';
-import logger from "morgan";
-import swaggerUi from "swagger-ui-express";
-import swaggerFile from "./swagger/output.json";
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger/output.json';
 
-import { router } from "./routes";
-import bodyParser from "body-parser";
-import path from "path";
+import { router } from './routes';
+import bodyParser from 'body-parser';
+import path from 'path';
 
 const PORT = 3333;
 
 const app = express();
 
-app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/files', express.static(path.join(process.cwd(), 'src/modules/extractor/invoices')))
-
-app.use("/api/", router);
 app.use(
-    "/api/docs",
-    swaggerUi.serve, 
-    swaggerUi.setup(swaggerFile)
+    '/files',
+    express.static(path.join(process.cwd(), 'src/modules/extractor/invoices'))
 );
+
+app.use('/api/', router);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(PORT, () => {
     console.log(`Server listening at port: ${PORT}`);
